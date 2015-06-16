79554ec568b3649f5755a5a26cc36052d477dea1
Celso Junior <celsojr100@gmail.com>
2015-06-16 18:16:43 -0300

    Interpretador de argumentos está funcionando

---
+++ colaborador.py
@@ -0,0 +1,7 @@
+class Colaborador:
+	def __init__(self,nome,email,endereco,senha,codigo):
+		self.nome = nome
+		self.email = email
+		self.endereco = endereco
+		self.senha = senha
+		self.codigo = codigo
---
+++ main.py
@@ -0,0 +1,22 @@
+import sys
+
+args = sys.argv
+entradas = []
+
+for i in range(0,5):
+	entradas.append("")
+	
+for i in range(0,len(args)):
+	if args[i] == "-e":
+		entradas[0] = args[i+1]
+	if args[i] == "-t":
+		entradas[1] = args[i+1]
+	if args[i] == "-p":
+		entradas[2] = args[i+1]
+	if args[i] == "-a":
+		entradas[3] = args[i+1]
+	if args[i] == "-r":
+		entradas[4] = args[i+1]
+
+print entradas
+
